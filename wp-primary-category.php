0a8dd30132877d24bd857f1386c44953fa390c54
Nathan Hangen <nathan@virtuousgiant.com>
2018-12-11 12:40:54 -0500

    Change folder/plugin base name in order to avoid conflict with primary-category plugin in WP repo.

---
+++ wp-primary-category.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Primary Category
+URI: https://github.com/nhangen/wp-primary-category
+Description: A WordPress plugin that gives authors an interface for use in selecting a primary post/custom post category.
+Version: 1.0
+Author: Nathan Hangen
+Author URI: http://nathanhangen.com
+License: GPL3
+*/
+
+
+?>
